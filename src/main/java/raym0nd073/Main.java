710b27a54794518ca795bf1b7aecf879377615e9
fAissss73 <149308037+fAissss73@users.noreply.github.com>
2023-10-29 21:31:09 +0300

    Update Main.java

--- src/main/java/raym0nd073/Main.java
+++ src/main/java/raym0nd073/Main.java
@@ -3,6 +3,7 @@ package raym0nd073;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello World!");
+        System.out.printf("Hello QAguru!");
         }
     }
 }

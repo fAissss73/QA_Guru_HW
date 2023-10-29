caeeb9c118903b3ce018c360c8dc8dad6285b626
Aleksandr Kuzmin <raym0nd0@yandex.ru>
2023-10-29 21:44:42 +0300

    Correct message

--- src/main/java/raym0nd073/Main.java
+++ src/main/java/raym0nd073/Main.java
@@ -3,5 +3,11 @@ package raym0nd073;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello World!");
+        System.out.printf("Hello QAguru!");
         }
+<<<<<<< HEAD
     }
+=======
+    }
+}
+>>>>>>> 710b27a54794518ca795bf1b7aecf879377615e9
